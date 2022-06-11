adb80552badcee891829b27d37a72290305cc6d9
Yasir Enes Bilmiş <100796277+yasirens@users.noreply.github.com>
2022-06-11 12:39:56 +0300

    Update and rename CircleParticleUtil.java to ParticleCircle.java

--- ParticleCircle.java
+++ ParticleCircle.java
@@ -1,4 +1,5 @@
-import org.bukkit.Color;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.block.BlockFace;
@@ -8,37 +9,45 @@ import org.jetbrains.annotations.NotNull;
 /**
  * @author ensyasir@gmail.com
  */
-public class CircleParticleUtil {
+@Getter
+@Setter
+public class ParticleCircle {
 
-    private CircleParticleUtil() {
-        throw new IllegalStateException();
+    private @NotNull Particle particle;
+    private int count;
+    private @NotNull Particle.DustOptions options;
+
+    public ParticleCircle(@NotNull Particle particle, int count, @NotNull Particle.DustOptions options) {
+        this.particle = particle;
+        this.count = count;
+        this.options = options;
     }
 
-    public static void displayCircle(@NotNull Location location, @NotNull BlockFace face, float radius, double t, double tplus) {
+    public void display(@NotNull Location location, @NotNull BlockFace face, float radius, double t, double tplus) {
         Vector direction = location.getDirection().normalize();
         float yaw = location.getYaw();
         if (face.getModX() == 0 && face.getModY() == 0) {
-            for (double t_ = 0; t_ <= t; t_ += tplus) {
+            for (double t_ = tplus; t_ <= t; t_ += tplus) {
                 double x = direction.getX() + radius * (float) Math.sin(t_);
                 double y = direction.getY() + radius * (float) Math.cos(t_);
                 location.add(x, y, 0);
-                location.getWorld().spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 0, 0, 0, 0, 1, new Particle.DustOptions(Color.SILVER, 5));
+                location.getWorld().spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, options);
                 location.subtract(x, y, 0);
             }
         } else if (face.getModY() == 0 && face.getModZ() == 0) {
-            for (double t_ = 0; t_ <= t; t_ += tplus) {
+            for (double t_ = tplus; t_ <= t; t_ += tplus) {
                 double y = direction.getY() + radius * (float) Math.cos(t_);
                 double z = direction.getZ() + radius * (float) Math.sin(t_);
                 location.add(0, y, z);
-                location.getWorld().spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 0, 0, 0, 0, 1, new Particle.DustOptions(Color.SILVER, 5));
+                location.getWorld().spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, options);
                 location.subtract(0, y, z);
             }
         } else if (face.getModX() == 0 && face.getModZ() == 0) {
-            for (double t_ = 0; t_ <= t; t_ += tplus) {
+            for (double t_ = tplus; t_ <= t; t_ += tplus) {
                 double x = direction.getX() + radius * (float) Math.sin(t_);
                 double z = direction.getZ() + radius * (float) Math.cos(t_);
                 location.add(x, 0, z);
-                location.getWorld().spawnParticle(Particle.REDSTONE, location.getX(), location.getY(), location.getZ(), 0, 0, 0, 0, 1, new Particle.DustOptions(Color.SILVER, 5));
+                location.getWorld().spawnParticle(particle, location.getX(), location.getY(), location.getZ(), count, options);
                 location.subtract(x, 0, z);
             }
         }
